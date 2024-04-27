4747d5e1770e4cfdae7c9117430d1a2ff9b9888d
Ray Peter <raypeter053@gmail.com>
2024-04-27 13:57:10 -0700

    Working on the whole look and feel 2

--- src/landing/register.js
+++ src/landing/register.js
@@ -642,7 +642,6 @@ const RegisterationHome = () => {
         </div>
       </section>
 
-
       <div className="modal fade" id="existModalCenter" tabindex="-1" role="dialog" aria-hidden="true">
         <div className="modal-dialog modal-dialog-centered" role="document">
           <div className="modal-content">
--- src/user/trading/trades.js
+++ src/user/trading/trades.js
@@ -1,10 +1,49 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const UserTrades = () => {
   return (
-    <div>
-      UserTrades
-    </div>
+    <>
+      <section class="">
+        <center>
+          <ul class="bg tabs container">
+            <li class="tab col s6" style={{"borderBottom":"3px solid black"}}>
+              <Link to="#open">
+                <span class="material-icons notranslate">hourglass_top</span>
+                <span>Open</span>
+              </Link>
+            </li>
+            <li class="tab  col s6">
+              <Link to="#closed" class="active">
+                <span class="material-icons notranslate">hourglass_full</span>
+                <span>Closed</span>
+              </Link>
+            </li>
+
+          </ul>
+        </center><br /><br />
+        <div class="container pc-scroll">
+          <div id="closed" class="active" style={{"display": "block"}}>
+            <ul class="collection">
+              <li class="collection-item app-py-2">
+                <p id="no-data" style={{ "textAlign": "center" }}>
+                  <span class="app-font-normal">NO CLOSED TRADES</span>
+                </p>
+              </li>
+            </ul>
+          </div>
+          <div id="open" style={{"display": "none"}}>
+            <ul class="collection">
+              <li class="collection-item app-py-2">
+                <p id="no-data" style={{ "textAlign": "center" }}>
+                  <span class="app-font-normal">NO OPEN TRADES</span>
+                </p>
+              </li>
+            </ul>
+          </div>
+        </div><br /><br /><br /><br />
+      </section>
+    </>
   )
 }
 
